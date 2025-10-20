c403f9f05cd60038ce69123e7a66d4e63fbf48c1
greatdom <www778005729@outlook.com>
2025-10-20 19:45:12 +0800

    在正式开始项目前要搓一个脚手架

---
+++ hinder/eureka/src/test/java/com/wddyxd/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wddyxd.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

bf9d8d0be7a6bc6a20226bc72cdd35683648ee41
greatdom <www778005729@outlook.com>
2025-10-25 15:47:10 +0800

    前后端交互为nginx负载均衡做调整

--- header-manager/src/utils/request.ts
+++ header-manager/src/utils/request.ts
@@ -7,7 +7,7 @@ import { ElMessage } from 'element-plus'
 const service = axios.create({
     // baseURL: process.env.BASE_API, // api 的 base_url
     //TODO 可以写配置文件
-    baseURL: 'http://localhost:10010',
+    baseURL: 'http://localhost:10010/api',
     timeout: 20000 // 请求超时时间
 })
 

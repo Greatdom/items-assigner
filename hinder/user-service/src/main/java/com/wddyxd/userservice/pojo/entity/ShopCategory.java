037880a7d010852937bd7f3b3883f377e2b6fddd
greatdom <www778005729@outlook.com>
2025-11-26 17:48:03 +0800

    设计了数据库,增加了模块的框架和相应的实体类

---
+++ hinder/user-service/src/main/java/com/wddyxd/userservice/pojo/entity/ShopCategory.java
@@ -0,0 +1,86 @@
+package com.wddyxd.userservice.pojo.entity;
+
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.util.Date;
+
+/**
+ * @program: items-assigner
+ * @description: description
+ * @author: wddyxd
+ * @create: 2025-11-26 16:31
+ **/
+
+@TableName("shop_category")
+public class ShopCategory {
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long id;
+    private String name;//分类名称
+    private String code;//分类标识
+    private Boolean isDeleted;//逻辑删除 1（true）已删除， 0（false）未删除
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    @Override
+    public String toString() {
+        return "ShopCategory{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                ", isDeleted=" + isDeleted +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
